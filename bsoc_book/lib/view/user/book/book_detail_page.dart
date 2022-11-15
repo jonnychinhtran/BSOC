d051573c0b4fd8039a6bece8d10ec36b57de921c
Le Ngoc Tai <104020709+tailn804@users.noreply.github.com>
2022-11-15 15:25:56 +0700

    add comment review in detail book

--- bsoc_book/lib/view/user/book/book_detail_page.dart
+++ bsoc_book/lib/view/user/book/book_detail_page.dart
@@ -315,9 +315,7 @@ class _DetailBookPageState extends State<DetailBookPage>
                             );
                           }),
                     ),
-                    Container(
-                      child: ListView(children: []),
-                    ),
+                    ReviewBook(),
                   ]),
                 ))
               ],
@@ -570,4 +568,132 @@ class _DownloadingDialogState extends State<DownloadingDialog> {
       ),
     );
   }
+}
+
+class ReviewBook extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                  child: Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: CircleAvatar(
+                    radius: 30,
+                    backgroundImage: NetworkImage(
+                      'https://avatars.githubusercontent.com/u/104020709?s=96&v=4',
+                    )),
+              )),
+              Expanded(
+                  child: Padding(
+                padding: const EdgeInsets.only(left: 10, right: 10),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Ngọc Tài',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      'Quyển kỷ yếu rất hay về chiến lược Marketing',
+                      textAlign: TextAlign.justify,
+                      style: TextStyle(fontWeight: FontWeight.normal),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    RatingBars(
+                      rating: 3.5,
+                      ratingCount: 12,
+                    )
+                  ],
+                ),
+              )),
+              SizedBox()
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class RatingBars extends StatelessWidget {
+  final double rating;
+  final double size;
+  int? ratingCount;
+
+  RatingBars({required this.rating, this.ratingCount, this.size = 18});
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> _startList = [];
+    int realNumber = rating.floor();
+    int partNumber = ((rating - realNumber) * 10).ceil();
+
+    for (int i = 0; i < 5; i++) {
+      if (i < realNumber) {
+        _startList.add(Icon(
+          Icons.star,
+          color: Colors.yellow,
+          size: size,
+        ));
+      } else if (i == realNumber) {
+        _startList.add(SizedBox(
+          height: size,
+          width: size,
+          child: Stack(
+            fit: StackFit.expand,
+            children: [
+              Icon(
+                Icons.star,
+                color: Colors.yellow,
+                size: size,
+              ),
+              ClipRect(
+                clipper: _Clipper(part: partNumber),
+                child: Icon(
+                  Icons.star,
+                  color: Colors.grey,
+                  size: size,
+                ),
+              ),
+            ],
+          ),
+        ));
+      } else {
+        _startList.add(Icon(
+          Icons.star,
+          color: Colors.grey,
+          size: size,
+        ));
+      }
+    }
+
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: _startList,
+    );
+  }
+}
+
+class _Clipper extends CustomClipper<Rect> {
+  final int part;
+  _Clipper({required this.part});
+  Rect getClip(Size size) {
+    return Rect.fromLTRB(
+        (size.width / 10) * part, 0.0, size.width, size.height);
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Rect> oldClipper) => true;
 }
