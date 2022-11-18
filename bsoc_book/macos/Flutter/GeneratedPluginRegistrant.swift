67f39578cf9c7eaade44abe3395fe10b82eeff98
tailn323 <182030912@hcmca.edu.vn>
2022-11-18 23:20:26 +0700

    remove notification

--- bsoc_book/macos/Flutter/GeneratedPluginRegistrant.swift
+++ bsoc_book/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,6 @@ import FlutterMacOS
 import Foundation
 
 import device_info_plus_macos
-import flutter_local_notifications
 import path_provider_macos
 import pdfx
 import shared_preferences_macos
@@ -15,7 +14,6 @@ import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
-  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   PdfxPlugin.register(with: registry.registrar(forPlugin: "PdfxPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
