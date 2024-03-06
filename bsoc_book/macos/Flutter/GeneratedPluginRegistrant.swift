27ca8140f41de6a7e4f5bfb46c25d183874bae3a
tailn804 <104020709+tailn804@users.noreply.github.com>
2024-03-07 01:08:41 +0700

    up version 1.1.5 & add package app_version_update

--- bsoc_book/macos/Flutter/GeneratedPluginRegistrant.swift
+++ bsoc_book/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,7 +8,7 @@ import Foundation
 import connectivity_plus
 import modal_progress_hud_nsn
 import network_info_plus_macos
-import package_info_plus_macos
+import package_info_plus
 import path_provider_macos
 import sentry_flutter
 import share_plus_macos
@@ -20,7 +20,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   ModalProgressHudNsnPlugin.register(with: registry.registrar(forPlugin: "ModalProgressHudNsnPlugin"))
   NetworkInfoPlusPlugin.register(with: registry.registrar(forPlugin: "NetworkInfoPlusPlugin"))
-  FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SentryFlutterPlugin.register(with: registry.registrar(forPlugin: "SentryFlutterPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
